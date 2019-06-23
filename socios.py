5b017ede573ecc45c2dd959cd6dad3a119163815
Muhammed Nazeem <39720973+NazeemNato@users.noreply.github.com>
2019-06-23 17:16:19 +0530

    Update socios.py

--- socios.py
+++ socios.py
@@ -5,22 +5,7 @@ import time
 
 count = 0
 while (count <= 5):
-    word_url = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
-    response = urllib.request.urlopen(word_url)
-    long_txt = response.read().decode()
-    words = long_txt.splitlines()
-    
-    upper_words = [word for word in words if word[0].isupper()]
-    name_words  = [word for word in upper_words if not word.isupper()]
-    one_name = ' '.join([name_words[random.randint(0, len(name_words))] for i in range(2)])
-    def rand_name():
-        name = ' '.join([name_words[random.randint(0, len(name_words))] for i in range(2)])
-        return name
-    for n in range(1):
-        name = rand_name()
-    def randString(length=10):
-        your_letters='abcdefghi'
-        return ''.join((random.choice(your_letters) for i in range(length)))
+
     from selenium.webdriver.common.keys import Keys
     from selenium import webdriver
     options = webdriver.ChromeOptions()
